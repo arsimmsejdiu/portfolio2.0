8818e70d73cc688d8984bf0f12fb0a9dc4e098b3
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-05-28 09:47:29 +0200

    little changes in the design

--- src/components/AnimatedText.component.jsx
+++ src/components/AnimatedText.component.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const AnimatedText = ({ text }) => (
   <h1 className="animated-text-container">
     {[...text].map((value, id) => (
-      <div key={id} className="animated-text-item">{value}</div>
+      <div key={id} className="animated-text-item">
+        {value}
+      </div>
     ))}
   </h1>
 );

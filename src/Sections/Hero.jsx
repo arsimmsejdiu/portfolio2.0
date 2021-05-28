4210d15da4ba3b6167b5d4fd54c42c325900a744
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-05-28 11:57:24 +0200

    Added about component so i don have to rewrite it again and again

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { More, Svg } from '../components/svg';
+import About from "../components/About";
 
 const Hero = () => {
   return (
     <div class="hero">
       <div class="content">
         <h1 class="stagger1">Fullstack Web Developer</h1>
-        <p class=" stagger1">
-          Versatile, results-driven professional with 2+ years comprehensive
-          experience Fullstack Developer adept in bringing forth expertise in
-          design, installation, testing and maintenance of software systems.
-          Equipped with a diverse and promising skill-set. Proficient in various
-          platforms, languages, and embedded systems. Experienced with the
-          latest cutting edge development tools and procedures. Able to
-          effectively self-manage during independent projects, as well as
-          collaborate as part of a productive team.
-        </p>
+        <About />
         <div class="meet stagger1">
           <span>👇</span>
           <p>Meet Arsim Sejdiu</p>
---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => (
+  <p class=" stagger1">
+    Versatile, results-driven professional with 2+ years comprehensive
+    experience Fullstack Developer adept in bringing forth expertise in design,
+    installation, testing and maintenance of software systems. Equipped with a
+    diverse and promising skill-set. Proficient in various platforms, languages,
+    and embedded systems. Experienced with the latest cutting edge development
+    tools and procedures. Able to effectively self-manage during independent
+    projects, as well as collaborate as part of a productive team.
+  </p>
+);
+
+export default About;

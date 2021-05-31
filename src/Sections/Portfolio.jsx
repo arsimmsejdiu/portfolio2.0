5c2ad9746a5a5ed0b92a482950eb91d2f8082cb7
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-05-31 11:08:53 +0200

    added loader while waitting for articles

--- src/Sections/Portfolio.jsx
+++ src/Sections/Portfolio.jsx
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Portfolios from "../components/Portfolios.component";
-import { FeatureSubtitle } from '../components/Paragraph';
+import { FeatureSubtitle } from "../components/Paragraph";
 import axios from "axios";
+import Loader from "../components/Loader";
 
-const url = 'https://memories-projects.herokuapp.com/posts';
+const url = "https://memories-projects.herokuapp.com/posts";
 
 const Portfolio = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    axios.get(url).then(res => {
-      setPosts(res.data)
-    }).catch(error => console.error(error))
-  },[])
+    axios
+      .get(url)
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <section id="work" className="portfolio">
@@ -20,18 +24,22 @@ const Portfolio = () => {
         <a href="/" className="featured-title">
           My Projects
         </a>
-        <FeatureSubtitle text="/ All My Projects With Live Link /"/>
+        <FeatureSubtitle text="/ All My Projects With Live Link /" />
       </div>
-      {posts.map(post => (
-        <Portfolios 
-          key={post.id}
-          title={post.title}
-          creator={post.creator}
-          message={post.message}
-          tags={post.tags}
-          selectedFile={post.selectedFile}
-        />
-      ))}
+      {!posts.length ? (
+        <Loader />
+      ) : (
+        posts.map((post) => (
+          <Portfolios
+            key={post.id}
+            title={post.title}
+            creator={post.creator}
+            message={post.message}
+            tags={post.tags}
+            selectedFile={post.selectedFile}
+          />
+        ))
+      )}
     </section>
   );
 };

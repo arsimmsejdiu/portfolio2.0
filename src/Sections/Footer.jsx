7bf5cd3784d872cf165e463bc6b0bcb744d5f9a5
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-09-16 09:19:51 +0200

    changed a little the footer

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-// import github from "../images/github.png";
-// import linkedin from "../images/linkedin.png";
-// import instagram from "../images/instagram.png";
-// import cv from "../images/cv.svg";
+import { Link } from "react-router-dom";
+import github from "../images/github.png";
+import linkedin from "../images/linkedin.png";
+import instagram from "../images/instagram.png";
+import cv from "../images/cv.svg";
 
 const Footer = () => {
   return (
@@ -12,6 +12,42 @@ const Footer = () => {
         <h3 className="pagination stagger1">
           {`Copyright by ${new Date().getFullYear()} Arsim Sejdiu`}
         </h3>
+          <div className="social-header pagination stagger1">
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/arsimmsejdiu"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={github} alt="dribbble" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/arsim-sejdiu-93447331/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={linkedin} alt="behance" />
+                </a>
+              </li>
+              <li>
+                <Link to="/">
+                  <img src={instagram} alt="insta" />
+                </Link>
+              </li>
+              <li>
+                <a
+                  href="https://drive.google.com/file/d/1bJhjGIvlsJJTPCxwsY0RvGEkHAk9r_Zv/view?usp=sharing"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={cv} alt="cv" />
+                </a>
+              </li>
+            </ul>
+          </div>
       </footer>
     </section>
   );

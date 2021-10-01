930918aefb88fac2fef20cdda49e908fa41d45a8
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-10-01 16:30:33 +0200

    Emaily js fix error

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import emailjs from "emailjs-com";
 
 const Message = () => {
+  const form = useRef();
+
   function sendEmail(e) {
     e.preventDefault();
 
@@ -9,7 +11,7 @@ const Message = () => {
       .sendForm(
         "service_lzkgmwg",
         "template_dlckpv5",
-        e.target,
+        form.current,
         "user_xbFWpXGeohdx2okl6BH7y"
       )
       .then(
@@ -27,15 +29,38 @@ const Message = () => {
       <div className="featured-title stagger1">
         <p>Send a DM for Collaboration</p>
       </div>
-      <form className="stagger1" onSubmit={sendEmail}>
+      <form className="stagger1" ref={form} onSubmit={sendEmail}>
         <label className="subtitle">Your Name</label>
-        <input type="text" name="user_name" placeholder="Your name.." required/>
+        <input
+          type="text"
+          name="user_name"
+          placeholder="Your name.."
+          required
+        />
+
+        <label className="subtitle">Last Name</label>
+        <input
+          type="text"
+          name="user_lastname"
+          placeholder="Your last name.."
+          required
+        />
 
         <label className="subtitle">Your Email</label>
-        <input type="email" name="user_email" placeholder="Your email.." required/>
+        <input
+          type="email"
+          name="user_email"
+          placeholder="Your email.."
+          required
+        />
 
         <label className="subtitle">Subject</label>
-        <input type="text" name="user_subject" placeholder="Your subject.." required/>
+        <input
+          type="text"
+          name="user_subject"
+          placeholder="Your subject.."
+          required
+        />
 
         <labe className="subtitle" for="message">
           Your Message
